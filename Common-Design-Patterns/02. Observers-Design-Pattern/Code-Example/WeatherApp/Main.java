0016715af5864210a5b2121a1d32f7a09ebaedd3
prathamesh <prathameshlendghar78@gmail.com>
2026-01-27 03:02:29 +0530

    Didn't add observable in the Observer and also the register and deregister functionality

--- Common-Design-Patterns/02. Observers-Design-Pattern/Code-Example/WeatherApp/Main.java
+++ Common-Design-Patterns/02. Observers-Design-Pattern/Code-Example/WeatherApp/Main.java
@@ -2,23 +2,33 @@ package WeatherApp;
 
 public class Main {
     public static void main(String[] args){
+        // Creating Observable
+        WeatherForecast weatherForecastStation = new WeatherForecast();
+
+        // Creating Observers
         WeatherObserverDisplay display1 = new WeatherObserverDisplay("display1");
-        WeatherObserverDisplay display2 = new WeatherObserverDisplay("display2");
+        WeatherObserverDisplay display2 = new WeatherObserverDisplay("display2", weatherForecastStation);   //Constructor level observable setting
         WeatherObserverDisplay display3 = new WeatherObserverDisplay("display3");
         WeatherObserverDisplay display4 = new WeatherObserverDisplay("display4");
 
 
-        WeatherForecast weatherForecastStation = new WeatherForecast();
+        // Registering the Observable in Observers separately
+        display1.setObservable(weatherForecastStation);
+        display3.setObservable(weatherForecastStation);
+        display4.setObservable(weatherForecastStation);
 
-        weatherForecastStation.addObserver(display1);
-        weatherForecastStation.addObserver(display2);
-        weatherForecastStation.addObserver(display3);
-        weatherForecastStation.addObserver(display4);
+        // Registering everyone
+        display1.registerObserver();
+        display2.registerObserver();
+        display3.registerObserver();
+        display4.registerObserver();
 
-        weatherForecastStation.notifyObserver();
 
-        weatherForecastStation.removeObserver(display4);
-        weatherForecastStation.notifyObserver();
+        //Checking the work of Observable notifying
+        weatherForecastStation.updateData((float)2.4,4,5);
+
+        display4.deRegisterObserver();
+        weatherForecastStation.updateData(4,3,6);
     }
 
 }
--- Common-Design-Patterns/02. Observers-Design-Pattern/Code-Example/WeatherApp/WeatherObserverDisplay.java
+++ Common-Design-Patterns/02. Observers-Design-Pattern/Code-Example/WeatherApp/WeatherObserverDisplay.java
@@ -4,9 +4,19 @@ public class WeatherObserverDisplay implements IObserver, IDisplay{
     private float temperature;
     private float humidity;
     private final String name;
+    private IObservable observable;
 
     WeatherObserverDisplay(String name){
         this.name = name;
+    }
+
+    WeatherObserverDisplay(String name, IObservable observable){
+        this.observable = observable;
+        this.name = name;
+    }
+
+    public void setObservable(IObservable observable){
+        this.observable = observable;
     }
 
     @Override
@@ -17,10 +27,19 @@ public class WeatherObserverDisplay implements IObserver, IDisplay{
         display();
     }
 
+    @Override
     public void display(){
         System.out.println(name + ": ");
         System.out.println("Temperature: " + this.temperature);
         System.out.println("Humidity: " + this.humidity);
         System.out.println();
+    }
+
+    public void registerObserver(){
+        this.observable.addObserver(this);
+    }
+
+    public void deRegisterObserver(){
+        this.observable.removeObserver(this);
     }
 }
